d598f3f397404325e91fc667d2ad2bc1ef3cb232
Radim Spetlik <spetlrad@fel.cvut.cz>
2025-04-09 10:17:40 +0200

    start.sh correction

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn --workers 10 --timeout 3600 --bind 0.0.0.0:80 --preload --log-config /opt/project/gunicorn_logging.conf -m 007 wsgi:flask_app
+gunicorn --workers 10 --timeout 3600 --bind 0.0.0.0:80 --preload --log-config /opt/project/gunicorn_logging.conf -m 007 wsgi:app
